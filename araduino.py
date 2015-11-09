094a57a43925bc6084a88d06fb45f36d1e31a687
duncan <duncan@suttree.com>
2015-11-09 14:56:44 +0000

    added command line flags
    -h and -d for help info and debug info

--- araduino.py
+++ araduino.py
@@ -1,75 +1,97 @@
+import os, sys, getopt
 import random
-import os, sys
 
-dice_roll = random.choice([1,2,3,4,5,6])
-if dice_roll < 6:
-  sys.exit()
+def usage():
+  print "araduino.py -h (help) -d (debug)"
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'python-musical'))
+def main(argv):
+  try:
+    opts, args = getopt.getopt(argv, "hd", ['help', 'debug'])
+  except getopt.GetoptError:
+    usage()
+    sys.exit(2)
 
-from musical.theory import Note, Scale, Chord
-from musical.audio import effect, playback
+  global _debug
+  _debug = 0
+  for opt, arg in opts:
+    if opt in ("-h", "--help"):
+      usage()
+      sys.exit()
+    elif opt == '-d':
+      _debug = 1
 
-from timeline import Hit, Timeline
+  dice_roll = random.choice([1,2,3,4,5,6])
+  if (dice_roll < 6 and _debug <> 1):
+    sys.exit()
 
-# Define key and scale
-#key = Note(random.choice(Note.NOTES))
-key = Note((random.choice(Note.NOTES), random.choice([0,1,2,3,4])))
+  sys.path.append(os.path.join(os.path.dirname(__file__), 'python-musical'))
 
-scales = ['major', 'minor', 'melodicminor', 'harmonicminor', 'pentatonicmajor', 'bluesmajor', 'pentatonicminor', 'bluesminor', 'augmented', 'diminished', 'wholehalf', 'halfwhole', 'wholetone', 'augmentedfifth', 'japanese', 'oriental', 'ionian', 'dorian', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian']
-scale = Scale(key, random.choice(scales))
+  from musical.theory import Note, Scale, Chord
+  from musical.audio import effect, playback
 
-print key
-print scale
+  from timeline import Hit, Timeline
 
-# Grab progression chords from scale starting at the octave of our key
-progression = Chord.progression(scale, base_octave=key.octave)
+  # Define key and scale
+  #key = Note(random.choice(Note.NOTES))
+  key = Note((random.choice(Note.NOTES), random.choice([0,1,2,3,4])))
 
-time = 0.0 # Keep track of currect note placement time in seconds
+  scales = ['major', 'minor', 'melodicminor', 'harmonicminor', 'pentatonicmajor', 'bluesmajor', 'pentatonicminor', 'bluesminor', 'augmented', 'diminished', 'wholehalf', 'halfwhole', 'wholetone', 'augmentedfifth', 'japanese', 'oriental', 'ionian', 'dorian', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian']
+  scale = Scale(key, random.choice(scales))
 
-timeline = Timeline()
+  print key
+  print scale
 
-# Add progression to timeline by arpeggiating chords from the progression
-#for index in [0, 2, 3, 1]:
-#  chord = progression[index]
-#  root, third, fifth = chord.notes
-#  arpeggio = [root, third, fifth, third]
-#  for i, interval in enumerate(arpeggio):
-#    ts = float(i * 2) / len(arpeggio)
-#    timeline.add(time + ts, Hit(interval, 1.0))
-#  time += 2.0
+  # Grab progression chords from scale starting at the octave of our key
+  progression = Chord.progression(scale, base_octave=key.octave)
 
-chord = progression[0]
+  time = 0.0 # Keep track of currect note placement time in seconds
 
-notes = [0, 1, 2]
+  timeline = Timeline()
 
-for i, interval in enumerate([0.8, 0.4, 0.4, 0.2, 0.2]):
-  random_note = random.choice(notes)
-  random_transpose = random.choice([0, 12, 24])
+  # Add progression to timeline by arpeggiating chords from the progression
+  #for index in [0, 2, 3, 1]:
+  #  chord = progression[index]
+  #  root, third, fifth = chord.notes
+  #  arpeggio = [root, third, fifth, third]
+  #  for i, interval in enumerate(arpeggio):
+  #    ts = float(i * 2) / len(arpeggio)
+  #    timeline.add(time + ts, Hit(interval, 1.0))
+  #  time += 2.0
 
-  note = chord.notes[random_note].transpose(random_transpose)
+  chord = progression[0]
 
-  time = time + interval
-  timeline.add(time, Hit(note, interval))
+  notes = [0, 1, 2]
 
-# Strum out root chord to finish
-timeline.add(time + 0.0, Hit(chord.notes[0], 2.0))
-timeline.add(time + 0.1, Hit(chord.notes[1], 2.0))
-timeline.add(time + 0.2, Hit(chord.notes[2].transpose(12), 2.0))
+  for i, interval in enumerate([0.8, 0.4, 0.4, 0.2, 0.2]):
+    random_note = random.choice(notes)
+    random_transpose = random.choice([0, 12, 24])
 
-print "Rendering audio..."
+    note = chord.notes[random_note].transpose(random_transpose)
 
-data = timeline.render()
+    time = time + interval
+    timeline.add(time, Hit(note, interval))
 
-#data = effect.chorus(data, freq=3.14159)
-#data = effect.tremolo(data, freq=3.14159)
-#data = effect.flanger(data, freq=3.14159)
+  # Strum out root chord to finish
+  timeline.add(time + 0.0, Hit(chord.notes[0], 2.0))
+  timeline.add(time + 0.1, Hit(chord.notes[1], 2.0))
+  timeline.add(time + 0.2, Hit(chord.notes[2].transpose(12), 2.0))
 
-# Reduce volume to 25%
-data = data * 0.25
+  print "Rendering audio..."
 
-print "Playing audio..."
+  data = timeline.render()
 
-playback.play(data)
+  #data = effect.chorus(data, freq=3.14159)
+  #data = effect.tremolo(data, freq=3.14159)
+  #data = effect.flanger(data, freq=3.14159)
 
-print "Done!"
+  # Reduce volume to 25%
+  data = data * 0.25
+
+  print "Playing audio..."
+
+  playback.play(data)
+
+  print "Done!"
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
